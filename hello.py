5893b555adb6b14777dd77c4c90538eecdb3a88b
Brad Nguyen <brad@192-168-1-108.tpgi.com.au>
2021-01-03 18:20:29 +1100

    Add data folder

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 from flask import Flask, Markup, redirect, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="data")
 
 @app.route('/')
 def hello_world():
